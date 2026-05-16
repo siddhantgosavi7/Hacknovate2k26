3ec47cd293695525278f581c0ad1d503a24e6d92
Siddhant Gosavi <109088765+siddhantgosavi7@users.noreply.github.com>
2026-05-17 05:04:15 +0530

    Delete backend directory

--- backend/src/routes/analyticsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getDashboardAnalytics } = require("../controllers/analyticsController");
-const { protect } = require("../middleware/authMiddleware");
-
-const router = express.Router();
-
-router.get("/dashboard", protect, getDashboardAnalytics);
-
-module.exports = router;
