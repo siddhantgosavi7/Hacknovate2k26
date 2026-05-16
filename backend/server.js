78bcedd7975bd8efd3183e099428dacaac24c4af
CalmFlea <prathamesh.chougale.4425@gmail.com>
2026-05-16 10:35:59 +0530

    user input-> backend

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,16 @@ app.use(express.json());
 app.post("/analyze", async (req, res) => {
 
     const item = req.body.item;
+    console.log(item);
 
     res.json({
         waste: "Plastic Bottle",
         recyclable: true,
         ecoScore: 87,
-        guidance: "Use Blue Bin"
+        guidance: "Use Blue Recycling Bin",
+        pointsEarned: 20,
+        confidence: "94%",
+        carbonSaved: "1.4kg CO₂"
     });
 
 });
--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,9 @@
 // @ts-nocheck
 async function analyzeWaste() {
 
+    const item = document.getElementById("wasteInput").value;
+    console.log(item);
+
     const response = await fetch(
         "http://localhost:3000/analyze",
         {
@@ -11,7 +14,7 @@ async function analyzeWaste() {
             },
 
             body: JSON.stringify({
-                item: "Bottle"
+                item: item
             })
         }
     );
@@ -20,7 +23,13 @@ async function analyzeWaste() {
 
     console.log(data);
 
-    document.getElementById("result").innerText =
-        `${data.waste} | Eco Score: ${data.ecoScore}`;
+    document.getElementById("result").innerHTML = `
+    <h2>${data.waste}</h2>
+    <p>Eco Score: ${data.ecoScore}</p>
+    <p>Guidance: ${data.guidance}</p>
+    <p>Points Earned: +${data.pointsEarned}</p>
+    <p>Confidence: ${data.confidence}</p>
+    <p>Carbon Saved: ${data.carbonSaved}</p>
+`;
 
 }
